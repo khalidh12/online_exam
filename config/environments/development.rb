1e4e7cb8e0e0cee0cf0ad26784f4550528b9e4f8
entersoft <apps@entersoft.in>
2017-04-06 02:20:03 +0530

    ecet completed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
     user_name:            'riseyuvaservices@gmail.com',
     password:             'aslamsaleem',
     authentication:       'plain',
