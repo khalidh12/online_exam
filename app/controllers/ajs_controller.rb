5b52181a1c143bac1c24b1f969c6ab0b49218400
entersoft <apps@entersoft.in>
2017-04-19 17:45:53 +0530

    some changes

--- app/controllers/ajs_controller.rb
+++ app/controllers/ajs_controller.rb
@@ -1,5 +1,28 @@
 class AjsController < ApplicationController
   def index
-  	@questions = Ecet.all
-  end
+  # 	@users=User.all
+  # 	@sub=Submitque.all
+  # 	@ecet = Ecet.all
+  # 	@users.each do |u|
+  # 		subject=["ece", "eee", "cse", "civil"]
+  		
+  # 		subject.each do |subj|
+  # 		@i=0
+  # 		@ss = subj
+  # 		@sub = Submitque.where(:userid => u.id, :subject => subj)
+  		
+  # 		@sub.each do |s|
+  # 			@q = Ecet.find(s.question)
+  # 			if s.answer == @q.t
+  # 				@i+=1
+  				
+  # 			end	
+  # 		end
+  		
+  		
+  # 		puts "User:#{u.id}  True:#{@i} atmpt:#{@sub.count} subject:#{subj}"
+  		
+  # 		end
+  # 	end
+  # end
 end
