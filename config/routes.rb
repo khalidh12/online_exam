1b0fc88fe613c42d497ad31f1179d5794379ee5f
entersoft <apps@entersoft.in>
2017-05-06 16:52:23 +0530

    ssc results

--- config/routes.rb
+++ config/routes.rb
@@ -70,7 +70,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-    root 'home#index'
+    root 'blogs#adminmain'
     
     devise_for :users, :controllers => { :registrations => 'registrations'}
     devise_scope :user do
