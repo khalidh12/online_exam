21867434ca8c35aabebcebd3c587058239417f97
khalidh sm <khalidhmhd@gmail.com>
2020-03-11 17:25:40 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            'riseyuvaservices@gmail.com',
-    password:             'aslamsaleem',
+    user_name:            'test@gmail.com',
+    password:             'test',
     authentication:       'plain',
     enable_starttls_auto: true  }
 
